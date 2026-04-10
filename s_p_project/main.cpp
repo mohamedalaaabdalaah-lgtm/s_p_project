11e7faeee9b871ef05b8c5f3c37d9801ac15884b
mohamed <mohamedlaaaalmpcee>
2026-04-11 00:23:12 +0200

    ii

--- s_p_project/main.cpp
+++ s_p_project/main.cpp
@@ -1,11 +1,50 @@
+//list of includes in main.cpp
 #include "stdafx.h"
 #include "s_p_project.h"
 #include <QtWidgets/QApplication>
+#include <string>
+#include <iostream>;
+using namespace std;
+
+
+
+struct seats
+{
+	int rows;
+	char columns;
+};
+
+//plane struct
+struct plane
+{
+    string model;
+    int code;
+    
+};
+
+
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     s_p_project window;
     window.show();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return app.exec();
 }
